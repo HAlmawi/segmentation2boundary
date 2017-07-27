7641ce279c30c9a3cbdc5a57a8297b3b41159fa9
HAlmawi <hasan.almawi@gmail.com>
2017-07-26 22:09:50 -0400

    Added change color function

---
+++ change_color.m
@@ -0,0 +1,17 @@
+function change_color(folder_name,save_folder,prev_color,new_color)
+    images = dir(folder_name,'/*.png');
+    for i=1:size(images,1)
+        im = imread(strcat(folder_name,'/',images(i).name));
+        for x=1:size(im,1)
+            for y=1:size(im,2)
+                if(squeeze(im(x,y,:))==squeeze(prev_color(:)))
+                    im(x,y,1) = new_color(1);
+                    im(x,y,2) = new_color(2);
+                    im(x,y,3) = new_color(3);
+                end
+            end
+        end
+        imwrite(im,strcat(save_folder,'/',images(i).name));
+    end
+end
+
